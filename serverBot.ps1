eeef294d387eb4c6a54e74b6253775b351923e2f
Jacob Kelley <exaybachay.ak@gmail.com>
2017-02-26 13:37:27 -0900

    Update serverbot.ps1
    Modified functionality to no longer look at statuscode, and instead rely on select-string (regex) functionality to determine if the site is functioning properly.
    
    Added more extensive comments to start implementing best practices in my scripts/programs as an exercise in complete/thorough coding practices.

--- serverBot.ps1
+++ serverBot.ps1
@@ -1,13 +1,15 @@
-#make sure there is a directory to store secure creds in
+# Make sure there is a directory to store secure creds in - create one if it doesn't exist
 if(!(Test-Path -Path C:\temp )){
     New-Item -ItemType Directory -Force -Path C:\temp
 }
 
-#receive creds from user and store as variable
-#$username = Read-Host -Prompt "Please enter the email address you want to send from:"
+# Receive creds from user and store as variable
+#    Disabled below line because it would prevent the script from running automatically at startup
+#    Left this as a comment, however, so that if someone wants to use it, it can easily be added back
+#    $username = Read-Host -Prompt "Please enter the email address you want to send from:"
 $username = "youraccount@domain.com"
 
-#Create a file to store your email password
+# Create a file to store your email password
 if(!(Test-Path -Path C:\temp\securestring.txt )){
     read-host -assecurestring "Please enter your password" | convertfrom-securestring | out-file C:\temp\securestring.txt
 }
@@ -15,25 +17,29 @@ $password = cat C:\temp\securestring.txt | convertto-securestring
 $mycreds = new-object -typename System.Management.Automation.PSCredential `
          -argumentlist $username, $password
 
+# Creating variable to detect whether or not a support email has been generated.  
+# This acts as a flag to ensure tickets don't get repeatedly generated while the incident is ongoing, and can be reset once connectivity is re-established
 $emailsent = 'na'
-#infinite loop for calling connect function
+# Infinite loop for calling connect function
 while(1)
 {
+    # Get all of your time info to use later for tracking script functions
     $a = Get-Date
     $hour = $a.Hour
     $minute = $a.Minute
+    # Send an email to an account you monitor, to ensure that the script is running every day.  
     if ($hour -eq '7' -And $Minute -gt 44) {
         Send-MailMessage -To "recipient1@domain.com" -Cc "recipient2@domain.com" -SmtpServer "smtp.office365.com" -Credential $mycreds -UseSsl "Serverbot is running :)" -Port "587" -Body "The script is still running" -From $username -BodyAsHtml
     }
+    # Get the website and statuscode, then look at the content to ensure the word you want to see is in it ("Generous" is the one I used most recently)
     $webrequest = (Invoke-WebRequest -URI "https://monitoredserver.com" -UseBasicParsing -TimeoutSec 60)
-    $statuscode = $webrequest.StatusCode
     write-host $a
-    write-host $statuscode
     $content = $webrequest.content
     $siteup = $content.tostring() -split "[`r`n]" | select-string -Pattern "generous"
 
-    #if status code is not 200, send an email to support or do nothing if email has been sent
-    if ($statuscode -ne 200) {
+    # If the script cannot download the page and find a word you want to see, email support or do nothing if flag is set
+    #    Was previously using statuscode for this functionality, but realized that the site could be up but the service unavailable, like in a DoS situation, or a crashed process
+    if (!$siteup) {
         if ($emailsent -ne 'True') {
             Send-MailMessage -To "recipient1@domain.com" -Cc "recipient2@domain.com","recipient3@domain.com" -SmtpServer "smtp.office365.com" -Credential $mycreds -UseSsl "Server is DOWN!" -Port "587" -Body "This is an automatically generated message.<br>Please check on the status of your server, as it appears to be down currently.<br> You may need to reboot the service and/or server to get it back up and running.<br>Best regards<br><b>Your Server Support Bot</b>" -From $username -BodyAsHtml
             $emailsent = 'True'
@@ -43,17 +49,13 @@ while(1)
         }
     }
 
-    #if you don't receive anything, email support
-    elseif(!$siteup){
-        Send-MailMessage -To "recipient1@domain.com" -Cc "recipient2@domain.com","recipient3@domain.com" -SmtpServer "smtp.office365.com" -Credential $mycreds -UseSsl "Server is DOWN!" -Port "587" -Body "This is an automatically generated message.<br>Please check on the status of your server, as it appears to be down currently.<br> You may need to reboot the service and/or server to get it back up and running.<br>Best regards<br><b>Your Server Support Bot</b>" -From $username -BodyAsHtml
-        $emailsent = 'True'
-    }
-    #if status code is 200, don't do anything just say it's working
+    # If you can see the content from $siteup, then you have to be getting a 200 status, and all is working.
+    #    When this runs, reset $emailsent to False to ensure the next time the site is down, it will work properly
     else {
         Write-Host "Server is returning a 200 status, and expected text exists.  We should be good here!"
         $emailsent = 'False'
     }
 
-    #sleep for a predetermined interval.  15 minutes/900 seconds is reasonable
+    # Sleep for a predetermined interval.  15 minutes/900 seconds is reasonable in most cases
     start-sleep -seconds 900
 }
